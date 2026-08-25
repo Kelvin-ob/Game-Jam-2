1a9ec26633b438da7608b4355cbd38a6fd1ed7da
selman2702 <selmankap10@gmail.com>
2026-08-25 15:32:23 +0200

    Add Player Controller

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+
+    private InputSystem_Actions inputActions;
+    private bool isSprinting;
+    private bool isCrouching;
+
+    void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+        inputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementNormalized()
+    {
+        Vector2 inputVector = inputActions.Player.Move.ReadValue<Vector2>();
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+
+    public bool getIsSprinting()
+    {
+        isSprinting = inputActions.Player.Sprint.IsPressed();
+        return isSprinting;
+    }
+
+    public bool getIsCrouching()
+    {
+        isCrouching = inputActions.Player.Crouch.IsPressed();
+        return isCrouching;
+    }
+
+    public Vector2 GetMousePos()
+    {
+        Vector2 mouseVector = inputActions.Player.Look.ReadValue<Vector2>();
+        return mouseVector;
+    }
+
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSens = 10f;
+    [SerializeField] private Transform playerBody;
+    [SerializeField] private GameInput gameInput;
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Vector2 mouseVector = gameInput.GetMousePos();
+        float mouseX = mouseVector.x * mouseSens * Time.deltaTime;
+        float mouseY = mouseVector.y * mouseSens * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Math.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        bool isCrouching = gameInput.getIsCrouching();
+        float targetY = isCrouching ? -0.5f : 0f;
+
+        // aktuelle Position holen
+        Vector3 pos = transform.localPosition;
+
+        // Y-Wert sanft angleichen
+        pos.y = Mathf.Lerp(pos.y, targetY, Time.deltaTime * 8f);
+
+        // neue Position setzen
+        transform.localPosition = pos;
+    }
+
+
+}
