a5c03a8c80ebd843dc3b505c421f4c4f2c53f721
selman2702 <selmankap10@gmail.com>
2026-09-06 19:56:47 +0200

    add pc actions

--- Assets/Scripts/PC_System/PC_Handler.cs
+++ Assets/Scripts/PC_System/PC_Handler.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 
@@ -12,33 +13,80 @@ public class PC_Handler : MonoBehaviour, IInteractable
     [SerializeField] private MouseLook mouseLook;
     [SerializeField] private GameObject crosshair;
 
+    [Header("Computer UI")]
+    [SerializeField] private GameObject computerUI;
+    [SerializeField] private PCUIFade pcUIFade;
 
+    [Header("Camera")]
+    [SerializeField] private float cameraWaitTime = 0.5f;
 
+    [SerializeField] private bool onComputer;
+    private bool isTransitioning;
 
+    private void Start()
+    {
+        onComputer = false;
+        isTransitioning = false;
+        computerUI.SetActive(false);
+    }
 
     private void Update()
     {
-        if (gameInput.getIsEscaped())
+        if (onComputer && !isTransitioning && gameInput.getIsEscaped())
         {
             ExitComputer();
         }
     }
 
-
     public void Interact()
     {
+        if (onComputer || isTransitioning)
+            return;
+
+        onComputer = true;
+        isTransitioning = true;
+
+        // Kamera zum PC wechseln
         CameraManager.SwitchCamera(pc_cam);
+
+        // Spieler deaktivieren
         player.SetMovementEnabled(false);
+        mouseLook.SetCanLook(false);
+
+        // Maus anzeigen
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = true;
-        mouseLook.SetCanLook(false);
+
+        // Andere UI ausblenden
         InteractPromptManager.Instance.hidePrompt();
         crosshair.SetActive(false);
+
+        // Computer UI erst nach dem Kamera-Zoom öffnen
+        StartCoroutine(OpenComputer());
+    }
+
+    private IEnumerator OpenComputer()
+    {
+        // Warten, bis die Kamera am PC angekommen ist
+        yield return new WaitForSeconds(cameraWaitTime);
+
+        // UI aktivieren
+        computerUI.SetActive(true);
+        pcUIFade.SetInteractionEnabled(false);
+
+        // UI langsam einblenden
+        pcUIFade.ShowUI();
+        yield return new WaitForSeconds(pcUIFade.FadeDuration);
+        pcUIFade.SetInteractionEnabled(true);
+        isTransitioning = false;
     }
 
     public void OnFocus()
     {
-        InteractPromptManager.Instance.showPrompt(promptText);
+        if (!onComputer)
+        {
+            InteractPromptManager.Instance.showPrompt(promptText);
+        }
     }
 
     public void OnLoseFocus()
@@ -48,12 +96,46 @@ public class PC_Handler : MonoBehaviour, IInteractable
 
     private void ExitComputer()
     {
+        if (!onComputer || isTransitioning)
+            return;
+
+        StopAllCoroutines();
+
+        StartCoroutine(CloseComputer());
+    }
+
+    private IEnumerator CloseComputer()
+    {
+        isTransitioning = true;
+        pcUIFade.SetInteractionEnabled(false);
+
+        // UI langsam ausblenden
+        pcUIFade.HideUI();
+
+        // Warten, bis Fade-Out fertig ist
+        yield return new WaitForSeconds(pcUIFade.FadeDuration);
+
+        // UI deaktivieren
+        computerUI.SetActive(false);
+
+        // Zurück zur FPS-Kamera
         CameraManager.SwitchCamera(fps_cam);
+
+        // Spieler wieder aktivieren
         player.SetMovementEnabled(true);
         mouseLook.SetCanLook(true);
+
+        // Crosshair wieder anzeigen
         crosshair.SetActive(true);
+
+        // Maus wieder sperren
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        // Interaktions-Prompt wieder anzeigen
         InteractPromptManager.Instance.showPrompt(promptText);
+
+        onComputer = false;
+        isTransitioning = false;
     }
 }
